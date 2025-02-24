70e200fbdad6203e3f5ee11e0b12a4d31f906a2d
Dharunraj10 <dharunraj.p@10decoders.in>
2025-02-24 15:59:18 +0530

    Update ListEmployee.js

--- employeefrontend/src/pages/ListEmployee.js
+++ employeefrontend/src/pages/ListEmployee.js
@@ -8,6 +8,7 @@ export default function ListEmployee() {
   useEffect(() => {
     axios.get('https://e6a3-14-195-132-106.ngrok-free.app/listEmployees')
       .then(response => {
+        console.log(response.json());
         console.log("API Response:", response.data);
         setEmployees(response.data);
       })
