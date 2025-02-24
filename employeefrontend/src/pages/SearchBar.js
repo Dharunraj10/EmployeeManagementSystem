20507e16ea574e729739ed024125cc3339c9693f
Dharunraj10 <dharunraj.p@10decoders.in>
2025-02-24 16:35:08 +0530

    Update SearchBar.js

--- employeefrontend/src/pages/SearchBar.js
+++ employeefrontend/src/pages/SearchBar.js
@@ -10,7 +10,7 @@ export default function SearchBar({ onSearchResult }) {
     if (!searchInput.trim()) return; 
 
     try {
-      const response = await axios.get(`https://e6a3-14-195-132-106.ngrok-free.app/search/${searchType}/${searchInput}`);
+      const response = await axios.get(`https://f932-14-195-132-106.ngrok-free.app/search/${searchType}/${searchInput}`);
       onSearchResult(response.data);
       setSearchInput(''); 
     } catch (error) {
