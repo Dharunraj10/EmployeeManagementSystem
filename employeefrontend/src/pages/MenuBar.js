b85c950b9976e9bda49ec405cea6ba6cde3d055e
Dharunraj10 <dharunraj.p@10decoders.in>
2025-02-24 14:52:30 +0530

    Update MenuBar.js

--- employeefrontend/src/pages/MenuBar.js
+++ employeefrontend/src/pages/MenuBar.js
@@ -52,7 +52,7 @@ export default function MenuBar({ onSearchResult }) {
 
           {observabilityOpen && (
             <>
-              <MenuItem component="a" href="http://localhost:8080/swagger-ui/index.html" target="_blank" onClick={handleMenuClose}>
+              <MenuItem component="a" href="https://bddc-14-195-132-106.ngrok-free.app/swagger-ui/index.html" target="_blank" onClick={handleMenuClose}>
                 Swagger
               </MenuItem>
               <MenuItem component="a" href="http://localhost:5601" target="_blank" onClick={handleMenuClose}>
